7522af5a7f176c47ca9717d8236699abd01a4018
Zach Walsh <ztwalsh@gmail.com>
2015-06-22 17:05:00 -0500

    Updates to the snippet.

--- snippets/js/star-behaviors.js
+++ snippets/js/star-behaviors.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
 	$('.v1-wrapper').click(function() {
 		$(this).children('.v1-histogram').fadeToggle(100);
+		return false;
+	});
+
+	$('html').click(function() {
+		$('.v1-histogram').fadeOut(200);
+	});
+
+	$('.v1-histogram').click(function(event){
+		event.stopPropagation();
 	});
 });
--- snippets/star.php
+++ snippets/star.php
@@ -32,97 +32,42 @@
 					</div>
 					<div class="v1-histogram">
 						<h3>Rating Breakdown</h3>
-						<table class="histogram">
-							<tr id="5" class="distro-bar">
-								<td class="rating">5 stars</td>
-								<td><div class="bar"><div style="width: 58%;"></div></div></td>
-								<td class="count">25</td>
-							</tr>
-							<tr id="4" class="distro-bar">
-								<td class="rating">4 stars</td>
-								<td><div class="bar"><div style="width: 14%;"></div></div></td>
-								<td class="count">7</td>
-							</tr>
-							<tr id="3" class="distro-bar">
-								<td class="rating">3 stars</td>
-								<td><div class="bar"><div style="width: 11%;"></div></div></td>
-								<td class="count">5</td>
-							</tr>
-							<tr id="2" class="inactive">
-								<td class="rating">2 stars</td>
-								<td><div class="bar"></div></td>
-								<td class="count">0</td>
-							</tr>
-							<tr id="1" class="distro-bar">
-								<td class="rating">1 star</td>
-								<td><div class="bar"><div style="width: 14%;"></div></div></td>
-								<td class="count">7</td>
-							</tr>
-						</table>
-						<div class="v1-read">
-							<a href="#">Read Reviews</a>
+						<div class="v1-inner">
+							<table class="histogram">
+								<tr id="5" class="distro-bar">
+									<td class="rating">5 stars</td>
+									<td><div class="bar"><div style="width: 58%;"></div></div></td>
+									<td class="count">25</td>
+								</tr>
+								<tr id="4" class="distro-bar">
+									<td class="rating">4 stars</td>
+									<td><div class="bar"><div style="width: 14%;"></div></div></td>
+									<td class="count">7</td>
+								</tr>
+								<tr id="3" class="distro-bar">
+									<td class="rating">3 stars</td>
+									<td><div class="bar"><div style="width: 11%;"></div></div></td>
+									<td class="count">5</td>
+								</tr>
+								<tr id="2" class="inactive">
+									<td class="rating">2 stars</td>
+									<td><div class="bar"></div></td>
+									<td class="count">0</td>
+								</tr>
+								<tr id="1" class="distro-bar">
+									<td class="rating">1 star</td>
+									<td><div class="bar"><div style="width: 14%;"></div></div></td>
+									<td class="count">7</td>
+								</tr>
+							</table>
 						</div>
-						<div class="v1-write">
-							<a href="#">Write a Review</a>
+						<div class="v1-inner">
+							<a class="v1-action" href="#">Read Reviews</a>
+							<a class="v1-action" href="#">Write a Review</a>
 						</div>
 					</div>
 				</div>
 			</article>
-
-			<!-- <article>
-				<h1>PDP Star Rating</h1>
-
-				<div class="v1-wrapper cf" href="#">
-					<div class="v1-summary cf">
-						<div class="v1-star-container cf">
-							<div class="v1-star v1-active"><img src="images/star.svg" height="16" width="16" /></div>
-							<div class="v1-star v1-active"><img src="images/star.svg" height="16" width="16" /></div>
-							<div class="v1-star v1-active"><img src="images/star.svg" height="16" width="16" /></div>
-							<div class="v1-star v1-active"><img src="images/star.svg" height="16" width="16" /></div>
-							<div class="v1-star v1-inactive"><img src="images/star.svg" height="16" width="16" /></div>
-							<div class="v1-score">4.1</div>
-						</div>
-						<div class="v1-read-write cf">
-							<div class="v1-read">
-								<a href="#">25 Reviews <i class="fa fa-angle-down"></i></a>
-							</div>
-							<div class="v1-write">
-								<a href="#">Write a Review</a>
-							</div>
-						</div>
-					</div>
-					<div class="v1-histogram">
-						<h3>Rating Breakdown</h3>
-						<table class="histogram">
-							<tr id="5" class="distro-bar">
-								<td class="rating">5 stars</td>
-								<td><div class="bar"><div style="width: 58%;"></div></div></td>
-								<td class="count">25</td>
-							</tr>
-							<tr id="4" class="distro-bar">
-								<td class="rating">4 stars</td>
-								<td><div class="bar"><div style="width: 14%;"></div></div></td>
-								<td class="count">7</td>
-							</tr>
-							<tr id="3" class="distro-bar">
-								<td class="rating">3 stars</td>
-								<td><div class="bar"><div style="width: 11%;"></div></div></td>
-								<td class="count">5</td>
-							</tr>
-							<tr id="2" class="inactive">
-								<td class="rating">2 stars</td>
-								<td><div class="bar"></div></td>
-								<td class="count">0</td>
-							</tr>
-							<tr id="1" class="distro-bar">
-								<td class="rating">1 star</td>
-								<td><div class="bar"><div style="width: 14%;"></div></div></td>
-								<td class="count">7</td>
-							</tr>
-						</table>
-					</div>
-				</div>
-			</article> -->
 		</section>
 	</body>
 </html>
