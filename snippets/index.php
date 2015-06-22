d09637aaf1805d1aefbf5cbae2f30f5965eba039
Zach Walsh <ztwalsh@gmail.com>
2015-06-22 16:36:57 -0500

    Adding new files for the snippets.

--- snippets/index.php
+++ snippets/index.php
@@ -5,7 +5,7 @@
 		<link href="css/recommended.css" rel="stylesheet" media="screen" type="text/css" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
 		<script src="js/jquery.js" type="text/javascript"></script>
-		<script src="js/behaviors.js" type="text/javascript"></script>
+		<script src="js/reco-behaviors.js" type="text/javascript"></script>
 	</head>
 	<body>
 		<?php //include('lib/include.header.php'); ?>
---
+++ snippets/js/star-behaviors.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+	$('.v1-wrapper').click(function() {
+		$(this).children('.v1-histogram').fadeToggle(100);
+	});
+});
