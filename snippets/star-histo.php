7d8ede2b71d225c0e23e83a86e49feff026dc231
Zach Walsh <ztwalsh@gmail.com>
2015-06-23 14:16:19 -0500

    Star and recommended score combination.

---
+++ snippets/star-histo.php
@@ -0,0 +1,67 @@
+<html>
+	<head>
+		<title>Snippet</title>
+		<link href="css/global.css" rel="stylesheet" media="screen" type="text/css" />
+		<link href="css/star-histo-snippet.css" rel="stylesheet" media="screen" type="text/css" />
+		<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
+		<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+		<script src="js/jquery.js" type="text/javascript"></script>
+	</head>
+	<body>
+		<?php //include('lib/include.header.php'); ?>
+		<section class="content">
+			<article>
+				<h1>PDP Star Rating</h1>
+
+				<div class="v1-wrapper cf" href="#">
+					<div class="v1-summary cf">
+						<div class="v1-star-container cf">
+							<div class="v1-star v1-active"><img src="images/star.svg" height="16" width="16" /></div>
+							<div class="v1-star v1-active"><img src="images/star.svg" height="16" width="16" /></div>
+							<div class="v1-star v1-active"><img src="images/star.svg" height="16" width="16" /></div>
+							<div class="v1-star v1-active"><img src="images/star.svg" height="16" width="16" /></div>
+							<div class="v1-star v1-inactive"><img src="images/star.svg" height="16" width="16" /></div>
+							<div class="v1-score">4.1</div>
+						</div>
+						<div class="v1-read-write cf">
+							<a class="v1-action" href="#">Read 37 Reviews</a>
+							<a class="v1-action" href="#">Write a Review</a>
+						</div>
+					</div>
+					<div class="v1-histogram">
+						<h3>Rating Breakdown</h3>
+						<div class="v1-inner">
+							<table class="histogram">
+								<tr id="5" class="distro-bar">
+									<td class="rating">5 stars</td>
+									<td><div class="bar"><div style="width: 58%;"></div></div></td>
+									<td class="count">25</td>
+								</tr>
+								<tr id="4" class="distro-bar">
+									<td class="rating">4 stars</td>
+									<td><div class="bar"><div style="width: 14%;"></div></div></td>
+									<td class="count">7</td>
+								</tr>
+								<tr id="3" class="distro-bar">
+									<td class="rating">3 stars</td>
+									<td><div class="bar"><div style="width: 11%;"></div></div></td>
+									<td class="count">5</td>
+								</tr>
+								<tr id="2" class="inactive">
+									<td class="rating">2 stars</td>
+									<td><div class="bar"></div></td>
+									<td class="count">0</td>
+								</tr>
+								<tr id="1" class="distro-bar">
+									<td class="rating">1 star</td>
+									<td><div class="bar"><div style="width: 14%;"></div></div></td>
+									<td class="count">7</td>
+								</tr>
+							</table>
+						</div>
+					</div>
+				</div>
+			</article>
+		</section>
+	</body>
+</html>
---
+++ snippets/star-reco.php
@@ -0,0 +1,39 @@
+<html>
+	<head>
+		<title>Snippet</title>
+		<link href="css/global.css" rel="stylesheet" media="screen" type="text/css" />
+		<link href="css/star-reco-snippet.css" rel="stylesheet" media="screen" type="text/css" />
+		<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
+		<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+		<script src="js/jquery.js" type="text/javascript"></script>
+		<script src="js/star-reco-behaviors.js" type="text/javascript"></script>
+	</head>
+	<body>
+		<?php //include('lib/include.header.php'); ?>
+		<section class="content">
+			<article>
+				<h1>PDP Star Rating</h1>
+
+				<div class="v1-wrapper cf" href="#">
+					<div class="v1-recommended">
+						<span>92</span>% <small>Recommended</small>
+					</div>
+					<div class="v1-summary cf">
+						<div class="v1-star-container cf">
+							<div class="v1-star v1-active"><img src="images/star.svg" height="16" width="16" /></div>
+							<div class="v1-star v1-active"><img src="images/star.svg" height="16" width="16" /></div>
+							<div class="v1-star v1-active"><img src="images/star.svg" height="16" width="16" /></div>
+							<div class="v1-star v1-active"><img src="images/star.svg" height="16" width="16" /></div>
+							<div class="v1-star v1-inactive"><img src="images/star.svg" height="16" width="16" /></div>
+							<div class="v1-score">4.1</div>
+						</div>
+						<div class="v1-read-write cf">
+							<a class="v1-action" href="#">Read 37 Reviews</a>
+							<a class="v1-action" href="#">Write a Review</a>
+						</div>
+					</div>
+				</div>
+			</article>
+		</section>
+	</body>
+</html>
