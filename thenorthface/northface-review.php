0e9da6a8306756ac422e83aa5b0afe3bb674996b
Zach Walsh <ztwalsh@gmail.com>
2015-08-28 10:56:40 -0500

    Working edits to northface css.

--- thenorthface/northface-review.php
+++ thenorthface/northface-review.php
@@ -337,7 +337,7 @@
 <script type="text/javascript" src="https://www.thenorthface.com/wcsstore/vf-storefront/js/metrics/zmetrics.js?v=10"></script>
 
 
-	<script type="text/javascript" src="https://www.thenorthface.com/libs.coremetrics.com/eluminate.js"></script>
+	<script type="text/javascript" src="//libs.coremetrics.com/eluminate.js"></script>
 	<script type="text/javascript" src="https://www.thenorthface.com/wcsstore/vf-storefront/js/metrics/cm-listener.js?v=10"></script>
 <!-- Google Tag Manager GTM-TSP4T9 --><script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-TSP4T9');</script><!-- End Google Tag Manager -->
 <script type="text/javascript" src="https://www.thenorthface.com/wcsstore/vf-storefront/js/metrics/gtm-listener.js?v=10"></script>
