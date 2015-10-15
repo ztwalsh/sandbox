c15e3ca753beeefa52ff877a62663f57019d78b9
Zach Walsh <ztwalsh@gmail.com>
2015-10-15 11:42:53 -0500

    Triggered by scrolling and made the widget smaller.

--- micro-feedback/js/behaviors.js
+++ micro-feedback/js/behaviors.js
@@ -1,4 +1,9 @@
 $(document).ready(function () {
+  $(window).scroll(function() {
+    $('.mfb-body').delay(2000).queue(function(next){
+      $(this).addClass('enabled');
+    });
+  });
   $('.mfb-answer > a').click(function() {
     $('.mfb-body').height($('.mfb-body').height());
 		$('.mfb-body').html('<div class="success"><img src="images/checkmark.jpg" class="success-icon" width="auto" height="auto" /></div>');
