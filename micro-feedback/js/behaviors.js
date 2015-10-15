dc610532b9f5bc5f6038bd0d1a9443320e91af68
Zach Walsh <ztwalsh@gmail.com>
2015-10-15 11:45:57 -0500

    Triggered by scrolling and made the widget smaller.

--- micro-feedback/js/behaviors.js
+++ micro-feedback/js/behaviors.js
@@ -2,19 +2,19 @@ $(document).ready(function () {
   $(window).scroll(function() {
     $('.mfb-body').delay(2000).queue(function(next){
       $(this).addClass('enabled');
-    });
-  });
-  $('.mfb-answer > a').click(function() {
-    $('.mfb-body').height($('.mfb-body').height());
-		$('.mfb-body').html('<div class="success"><img src="images/checkmark.jpg" class="success-icon" width="auto" height="auto" /></div>');
-    $('.success-icon').delay(200).queue(function(next){
-			$(this).addClass('open');
-			$(this).after($('<h3>Thanks for your feedback!</h3>').fadeIn(500));
-			setTimeout(function () {
-	        $('.mfb-body').fadeOut(100);
-	    }, 3000);
-		});
+      $('.mfb-answer > a').click(function() {
+        $('.mfb-body').height($('.mfb-body').height());
+    		$('.mfb-body').html('<div class="success"><img src="images/checkmark.jpg" class="success-icon" width="auto" height="auto" /></div>');
+        $('.success-icon').delay(200).queue(function(next){
+    			$(this).addClass('open');
+    			$(this).after($('<h3>Thanks for your feedback!</h3>').fadeIn(500));
+    			setTimeout(function () {
+    	        $('.enabled').removeClass('enabled');
+    	    }, 3000);
+    		});
 
-    return false;
+        return false;
+      });
+    });
   });
 });
