d5b070644559983b7dccd2cb8876479097e29dd7
Zach Walsh <ztwalsh@gmail.com>
2017-08-11 23:37:50 -0500

    media first prototype

--- media-first/index.php
+++ media-first/index.php
@@ -1,24 +1,11 @@
 <!DOCTYPE html>
 <html>
-	<?php
-		$page_title = '';
-		$page = 'home';
-		require('lib/include.head.php');
-	?>
+	<head>
+		<title>Add a Photo</title>
+	</head>
 	<body>
 
-		<?php require('lib/include.nav.php'); ?>
-
-		<section class="content text-center">
-			<section>
-				<img class="zach" height="" src="images/zachwalsh.jpg" width="" />
-				<h1 class="heading-1">Zach Walsh</h1>
-				<h2 class="heading-3 small">UX + Product Design</h2>
-			</section>
-			<section>
-				<a class="secondary dribbble" href="https://dribbble.com/zachwalsh"><i class="fa fa-dribbble"></i> Dribbble</a>
-				<a class="secondary twitter" href="https://twitter.com/ztwalsh"><i class="fa fa-twitter"></i> Twitter</a>
-			</section>
-		</section>
+		<?php require('lib/include.product.php'); ?>
+		<p>Test</p>
 	</body>
 </html>
