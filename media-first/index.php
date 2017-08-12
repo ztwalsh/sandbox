1dd1a2d043908650eb73bc32a19bd827070e7e01
Zach Walsh <ztwalsh@gmail.com>
2017-08-11 23:26:15 -0500

    media first prototype

---
+++ media-first/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+	<?php
+		$page_title = '';
+		$page = 'home';
+		require('lib/include.head.php');
+	?>
+	<body>
+
+		<?php require('lib/include.nav.php'); ?>
+
+		<section class="content text-center">
+			<section>
+				<img class="zach" height="" src="images/zachwalsh.jpg" width="" />
+				<h1 class="heading-1">Zach Walsh</h1>
+				<h2 class="heading-3 small">UX + Product Design</h2>
+			</section>
+			<section>
+				<a class="secondary dribbble" href="https://dribbble.com/zachwalsh"><i class="fa fa-dribbble"></i> Dribbble</a>
+				<a class="secondary twitter" href="https://twitter.com/ztwalsh"><i class="fa fa-twitter"></i> Twitter</a>
+			</section>
+		</section>
+	</body>
+</html>
