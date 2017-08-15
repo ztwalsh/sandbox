f1dd5baf163ecf5e1ddaeb65c58bd57777006613
Zach Walsh <ztwalsh@gmail.com>
2017-08-14 20:49:20 -0500

    media first prototype

--- media-first/index.php
+++ media-first/index.php
@@ -11,7 +11,7 @@
 			<div class="wrapper">
 				<h1 class="heading-1">Add a Photo</h1>
 				<h3 class="heading-4 small">Your photos help future shoppers make decisions on what they buy.</h3>
-				<input class="file" type="file" id="file_upload">
+				<input class="file" type="file" id="file_upload" multiple>
 				<label for="file_upload" class="secondary action add-image">
 					<p><img src="images/image-placeholder.png" /></p>
 					<p><span class="btn-primary full">Find Photos or Videos</span></p>
