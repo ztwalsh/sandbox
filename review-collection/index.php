a7533ca660644208577fbc974cb29d35aef3a902
Zach Walsh <ztwalsh@gmail.com>
2017-08-26 13:45:06 -0500

    Changed the title of the directory/

--- review-collection/index.php
+++ review-collection/index.php
@@ -8,10 +8,11 @@
 	<body>
 		<section class="review-form">
 			<div class="wrapper">
-        <h1 class="heading-1">Versions</h1>
-        <p><a href="standard/review.php">Standard Collection</a></p>
-        <p><a href="image-first/image.php">Image First Collection</a></p>
-        <p><a href="review-first/review.php">Review First Collection</a></p>
+        <p>&nbsp;</p>
+        <h1 class="heading-1">Review Collection Versions</h1>
+        <p class="heading-3"><a href="standard/review.php">Standard Collection</a></p>
+        <p class="heading-3"><a href="image-first/image.php">Image First Collection</a></p>
+        <p class="heading-3"><a href="review-first/review.php">Review First Collection</a></p>
 			</div>
 		</section>
 	</body>
