b7dc4ffafd3ab3f494c8e3378309ba0f5d749fbd
Zach Walsh <ztwalsh@gmail.com>
2017-08-26 13:43:56 -0500

    Changed the title of the directory/

--- review-collection/index.php
+++ review-collection/index.php
@@ -1,4 +1,18 @@
-<h2>Versions</h2>
-<p><a href="standard/review.php">Standard Collection</a></p>
-<p><a href="image-first/image.php">Image First Collection</a></p>
-<p><a href="review-first/review.php">Review First Collection</a></p>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Review Collection</title>
+		<?php require('lib/include.head.php'); ?>
+	</head>
+
+	<body>
+		<section class="review-form">
+			<div class="wrapper">
+        <h1 class="heading-1">Versions</h1>
+        <p><a href="standard/review.php">Standard Collection</a></p>
+        <p><a href="image-first/image.php">Image First Collection</a></p>
+        <p><a href="review-first/review.php">Review First Collection</a></p>
+			</div>
+		</section>
+	</body>
+</html>
