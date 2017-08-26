ebdcbab9be614b03cfa28593327328e7306264b8
Zach Walsh <ztwalsh@gmail.com>
2017-08-26 13:38:41 -0500

    Changed the title of the directory/

--- review-collection/lib/config.php
+++ review-collection/lib/config.php
@@ -2,12 +2,12 @@
   if ($_SERVER['HTTP_HOST'] == 'localhost:8000') {
     //if (!ini_get('display_errors')) { ini_set('display_errors', '1'); }
     //if (!ini_get('display_startup_errors')) { ini_set('display_startup_errors', '1'); }
-    $root 			= 'http://localhost:8000/sandbox.ztwalsh.com/media-first/';
+    $root 			= 'http://localhost:8000/sandbox.ztwalsh.com/review-collection/';
     $host 			= 'localhost';
     $username 		= 'root';
     $password 		= 'root';
   } elseif ($_SERVER['HTTP_HOST'] == 'x.ztwalsh.com') {
-    $root 			= 'http://x.ztwalsh.com/media-first/';
+    $root 			= 'http://x.ztwalsh.com/review-collection/';
     $host 			= 'localhost';
     $username 		= 'ztwalshdb';
     $password 		= 'Z#twrz843';
