d92d805d3bedbfe927e7a174522c733f600fba8b
Zach Walsh <ztwalsh@gmail.com>
2017-08-31 09:37:40 -0500

    Adding tracking for the forms

--- review-collection/lib/include.head.php
+++ review-collection/lib/include.head.php
@@ -1,6 +1,6 @@
+<?php require('include.tracking.php'); ?>
 <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
 <link href="<?php echo $root; ?>styles/styles.css" rel="stylesheet" type="text/css" />
 <link href="//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
 <script type="text/javascript" src="<?php echo $root; ?>js/jquery.js"></script>
 <script type="text/javascript" src="<?php echo $root; ?>js/behaviors.js"></script>
-<?php require('include.tracking.php'); ?>
