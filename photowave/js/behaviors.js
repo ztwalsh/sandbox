8bec1bba33aa82c442987160a994652d7b14b856
Zach Walsh <ztwalsh@gmail.com>
2018-06-12 22:55:17 -0500

    Adding files for the initial site push.

--- photowave/js/behaviors.js
+++ photowave/js/behaviors.js
@@ -4,8 +4,12 @@ $(document).ready(function() {
       scroll_pos = $(this).scrollTop();
       if(scroll_pos > 200) {
           $('nav').addClass('light');
+					$('nav.override').addClass('swap');
+					$('nav.override').removeClass('override');
       } else {
           $('nav').removeClass('light');
+					$('nav.swap').addClass('override');
+					$('nav.swap').removeClass('swap');
       }
   });
 });
